c500933beaa089477a3838523eb7e7beb1c553ca
Douglas Harrington Muhone <doug@dhmgroup.net>
2022-06-25 23:44:33 +0200

    added tablename to model

--- app/model/todo.py
+++ app/model/todo.py
@@ -8,11 +8,13 @@ class TodoModel(SQLModel):
 
 
 class Todo(TodoModel, table=True):
+    __tablename__ = "todos"
     id: Optional[int] = Field(default=None, primary_key=True)
 
 
 class TodoCreate(TodoModel):
     pass
+
 
 class TodoUpdate(TodoModel):
     note: Optional[str] = None
